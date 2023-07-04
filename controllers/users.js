a233efca9a780739029997353f5c1a4bae224fe3
terr0byte <il.fa201114@yandex.ru>
2023-07-04 22:52:22 +0300

    refactor

--- controllers/users.js
+++ controllers/users.js
@@ -32,7 +32,6 @@ module.exports.updateProfile = (req, res) => {
   User.findByIdAndUpdate(req.user._id, req.body, { new: true, runValidators: true })
     .then((user) => res.send({ data: user }))
     .catch((err) => {
-      console.log(err.name);
       if (err.name === 'TypeError' || err.name === 'ValidationError') return res.status(400).send({ message: 'Неправильный формат данных' });
       if (err.name === 'CastError') return res.status(404).send({ message: 'Запрашиваемый пользователь не найден' });
       return res.status(500).send({ message: 'Что-то пошло не так...' });
