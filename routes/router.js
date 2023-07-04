fc49eea9008756238296243310216cce0a9c50ca
terr0byte <il.fa201114@yandex.ru>
2023-07-05 00:00:54 +0300

    refactor

--- routes/router.js
+++ routes/router.js
@@ -1,4 +1,7 @@
 const router = require('express').Router();
+const {
+  ERROR_NOT_FOUND,
+} = require('../utils/constants');
 
 const {
   createUser,
@@ -28,5 +31,9 @@ router.patch('/users/me', updateProfile);
 router.patch('/users/me/avatar', updateAvatar);
 router.put('/cards/:cardId/likes', likeCard);
 router.delete('/cards/:cardId/likes', dislikeCard);
+
+router.patch('/404', (req, res) => {
+  res.status(ERROR_NOT_FOUND).send({ message: 'NOT FOUND' });
+});
 
 module.exports = router;
