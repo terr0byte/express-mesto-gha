f6775c5cb9decddc208adc69e7a1ff82c34fa056
terr0byte <il.fa201114@yandex.ru>
2023-07-09 09:49:07 +0300

    refactor

--- routes/router.js
+++ routes/router.js
@@ -36,7 +36,7 @@ router.post('/signup', celebrate({
     password: Joi.string().required().min(8),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string(),
+    avatar: Joi.string().uri(),
   }),
 }), createUser);
 
