1e31366b81ed55a84be6c70288e6883140cd863a
terr0byte <il.fa201114@yandex.ru>
2023-07-09 23:09:44 +0300

    refactor

--- controllers/users.js
+++ controllers/users.js
@@ -114,7 +114,7 @@ module.exports.login = (req, res, next) => {
           res.cookie('jwt', token, {
             maxAge: 3600000 * 24 * 7,
             httpOnly: true,
-          }).end();
+          }).send();
         });
     })
     .catch(() => {
